d421977e8639d00768f4d3d1fdd1a18676ab9b8d
fatdoge <020949@zjtlcb.com>
2019-10-09 10:37:56 +0800

    did I finish my sudoku?

--- Did I Finish my Sudoku.js
+++ Did I Finish my Sudoku.js
@@ -1,6 +1,47 @@
 function doneOrNot(board){
+  let rowMap = [], columnMap = [], regionMap = []
   //your code here
+  for(let i = 0;i < 9;i++) {
+    for(let j = 0;j < 9;j++) {
+      if(rowMap[i] && rowMap[i][board[i][j]]) {
+        return 'Try again!'
+      } else {
+        rowMap[i] = {}
+        rowMap[i][board[i][j]] = true
+      }
+
+      if(columnMap[j] && columnMap[j][board[j][i]]) {
+        return 'Try again!'
+      } else {
+        columnMap[j] = {}
+        columnMap[j][board[j][i]] = true
+      }
+
+      // 判断数独每一块是否符合规则 i从0=>8，共9块
+      if(regionMap[i] && regionMap[i][board[i*3+j%3][i*3+j%3]]) {
+        return 'Try again!'
+      } else {
+        regionMap[i] = {}
+        regionMap[i][board[i*3+j%3][j%3]] = true
+      }
+
+    }
+    rowMap = []
+    columnMap = []
+    regionMap = []
+  }
+  return 'Finished!'
+
 }
+
+// function doneOrNot(board){
+//   for(let i = 0; i < 9; i++) {
+//     if(board[i].sort().join('') !== [...new Set(board[i])].sort().join('')) {
+//       return 'Try again!'
+//     }
+//   }
+
+// }
 
 console.log(doneOrNot([[5, 3, 4, 6, 7, 8, 9, 1, 2], 
   [6, 7, 2, 1, 9, 5, 3, 4, 8],
@@ -21,3 +62,5 @@ console.log(doneOrNot([[5, 3, 4, 6, 7, 8, 9, 1, 2],
   [9, 0, 1, 5, 3, 7, 2, 1, 4],
   [2, 8, 7, 4, 1, 9, 6, 3, 5],
   [3, 0, 0, 4, 8, 1, 1, 7, 9]]), "Try again!");
+
+  // https://www.codewars.com/kata/53db96041f1a7d32dc0004d2
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # 刷题就完事儿了!
+<div align='center'>
+  <img src='https://www.codewars.com/users/FatDoge/badges/micro'/>
+</div>
